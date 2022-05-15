bb54b316744254968b2ef9b12800073549c6da29
RobertHernandezArenas <airbusjayrobert@gmail.com>
2022-05-15 12:44:27 +0200

    menu: imagenes optimizadas

---
+++ src/components/BannerInfo/BannerInfo.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="banner-info">
+        <div class="banner-info__margin">
+
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.banner-info {
+    display: flex;
+	justify-content: center;
+	background-color: #f1f1f1;
+	padding: 16px 0;
+}
+.banner-info__margin {
+	display: flex;
+	justify-content: space-between;
+	width: 80%;
+}
+</style>
